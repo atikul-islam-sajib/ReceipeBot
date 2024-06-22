56f2992bf199a743cd9c7b6e586f797b32bfb0dc
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-22 23:30:40 +0200

    utils file is added

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import joblib
+
+
+def dump(value=None, filename=None):
+    if (value is not None) and (filename is not None):
+        joblib.dump(value=value, filename=filename)
+
+    else:
+        raise Exception("value and filename must be provided".capitalize())
+
+
+def load(filename=None):
+    if filename is not None:
+        return joblib.load(filename=filename)
+
+    else:
+        raise Exception("Filename should be passed".capitalize())
